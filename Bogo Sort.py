084a5a6389ff8b3a8ed661fd7923d50da5d58dd1
Joni Ikävalko <118250563+joniik@users.noreply.github.com>
2024-01-11 09:36:14 +0200

    Add files via upload

---
+++ Bogo Sort.py
@@ -0,0 +1,36 @@
+import random
+
+random.seed(random.randint(0, 999999999))
+
+def Luolista(n):
+    return list(range(1, n + 1))
+
+def sekoitalista(L):
+    Sekoitettulista = L.copy()
+    for i in range(len(Sekoitettulista)):
+        indeksi = random.randint(0, len(Sekoitettulista) - 1)
+        Sekoitettulista[i], Sekoitettulista[indeksi] = Sekoitettulista[indeksi], Sekoitettulista[i]
+    return Sekoitettulista
+
+def sortattu(L):
+    print (L)
+    return all(L[i] <= L[i + 1] for i in range(len(L) - 1))
+    
+def sortti(L):
+    laskuri = 0
+    while not sortattu(L):
+        random.shuffle(L)
+        laskuri += 1
+    return L, laskuri
+
+
+luolista_result = Luolista(13)
+sekoitettu_result = sekoitalista(luolista_result)
+
+print("Luolista:", luolista_result)
+print("Sekoitettu lista:", sekoitettu_result)
+
+sorted_result, laskuri = sortti(sekoitettu_result)
+print("sortattu:", sorted_result)
+print("yrityksia:", laskuri , "Kpl")
+
